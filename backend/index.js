adda99701cfe24ff4f6dbab28e8535fec445f64b
ho4upitsu <ho4upitsu@gmail.com>
2025-02-14 13:21:50 +0200

    add mongodb connection and basic user model

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
+
+dotenv.config();
 
 const app = express();
+
+console.log(process.env.MONGO_URL);
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+        console.log("Error connecting to MongoDB: ", error);
+    });
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
