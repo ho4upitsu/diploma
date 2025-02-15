2abe629665cc27ca679d02cd488b54afa19d2830
ho4upitsu <ho4upitsu@gmail.com>
2025-02-15 20:13:32 +0200

    add basic code execution logic

--- backend/route/main.js
+++ backend/route/main.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const UserRoute = require("./user");
+const CodeEditorRoute = require("./codeeditor");
 
 router.use("/auth", UserRoute);
+router.use("/code", CodeEditorRoute);
 
 module.exports = router;
