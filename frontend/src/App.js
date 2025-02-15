af2469d07e783db5a91671891e7a7f25f2ddb85d
ho4upitsu <ho4upitsu@gmail.com>
2025-02-15 17:04:50 +0200

    fix routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
-import "./App.css";
-//import Login from "./pages/Login/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main/Main";
+import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import "./App.css";
 
 function App() {
     return (
-        <div className="App">
-            <Register />
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
