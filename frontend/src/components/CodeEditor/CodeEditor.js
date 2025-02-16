5b1016928080bbac894994184044be4ba73e6c17
ho4upitsu <ho4upitsu@gmail.com>
2025-02-16 20:06:53 +0200

    add fetch with code execution

--- frontend/src/components/CodeEditor/CodeEditor.js
+++ frontend/src/components/CodeEditor/CodeEditor.js
@@ -1,13 +1,27 @@
-// src/components/CodeEditor/CodeEditor.js
 import React, { useState } from "react";
 import MonacoEditor from "@monaco-editor/react";
 
 const CodeEditor = () => {
     const [code, setCode] = useState("// Напишіть свій код тут");
+    const [response, setResponse] = useState("");
 
-    const handleEditorChange = (value, event) => {
-        setCode(value);
+    const handleEditorChange = (value) => {
+        setCode(value || ""); // Уникаємо null або undefined
     };
+
+    function sendCode(event) {
+        event.preventDefault(); // Запобігає перезавантаженню сторінки
+        fetch("http://localhost:5000/code/execute", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ code }),
+        })
+            .then((response) => response.json())
+            .then((data) => setResponse(data))
+            .catch((error) => console.error(error));
+    }
 
     return (
         <div>
@@ -15,15 +29,16 @@ const CodeEditor = () => {
             <MonacoEditor
                 height="60vh"
                 width="100vh"
-                language="javascript" // Встановлює мову програмування
-                theme="vs-dark" // Тема редактора
-                value={code}
+                language="javascript"
+                theme="vs-dark"
+                defaultValue="// Напишіть свій код тут"
                 onChange={handleEditorChange}
-                options={{
-                    selectOnLineNumbers: true,
-                    automaticLayout: true,
-                }}
+                options={{ selectOnLineNumbers: true, automaticLayout: true }}
             />
+            <form onSubmit={sendCode}>
+                <button type="submit">Send</button>
+            </form>
+            {response && <p>{response}</p>}
         </div>
     );
 };
