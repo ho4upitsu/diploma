6620bccda48a1cc423a215a4471b39bfd0b9a2c9
ho4upitsu <ho4upitsu@gmail.com>
2025-02-21 23:58:19 +0200

    fixed scrolling on modules page

--- frontend/src/pages/Modules/Modules.js
+++ frontend/src/pages/Modules/Modules.js
@@ -1,11 +1,29 @@
+import { useEffect } from "react";
 import ModulesComponent from "../../components/Modules/Modules";
 import Topbar from "../../components/Topbar/Topbar";
+// import Sidebar from "../../components/Sidebar/Sidebar"; // Додаємо ще один компонент
+import styles from "./Modules.module.css"; // Підключаємо стилі
+
 function Modules() {
+    useEffect(() => {
+        // При вході на сторінку блокуємо скролінг основної сторінки
+        document.body.style.overflow = "hidden";
+
+        return () => {
+            // Після виходу зі сторінки повертаємо скролінг
+            document.body.style.overflow = "auto";
+        };
+    }, []);
     return (
-        <>
+        <div className={styles.pageContainer}>
             <Topbar />
-            <ModulesComponent />
-        </>
+            <div className={styles.content}>
+                {/* <Sidebar /> Наприклад, меню зліва */}
+                <div className={styles.modulesWrapper}>
+                    <ModulesComponent />
+                </div>
+            </div>
+        </div>
     );
 }
 
