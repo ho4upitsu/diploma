03222301f4fb22ce19dfa915128660ea6ea62d5b
ho4upitsu <ho4upitsu@gmail.com>
2025-02-23 10:49:45 +0200

    add gettaskgroupsbymoduleid logic

--- backend/route/taskGroup.js
+++ backend/route/taskGroup.js
@@ -4,6 +4,10 @@ const taskGroupController = require("../controller/taskGroup");
 router.post("/createTaskGroup", taskGroupController.createTaskGroup);
 router.get("/getAllTaskGroups", taskGroupController.getAllTaskGroups);
 router.get("/getTaskGroup/:id", taskGroupController.getTaskGroup);
+router.get(
+    "/getTaskGroupsByModuleId/:id",
+    taskGroupController.getTaskGropsByModuleId
+);
 //router.delete("/deleteTestTask/:id", testTaskController.deleteTestTask);
 
 module.exports = router;
