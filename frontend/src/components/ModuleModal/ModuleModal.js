f5f02187ff3f7172b3a0d86c45e451d17efa3787
ho4upitsu <ho4upitsu@gmail.com>
2025-02-26 16:55:10 +0200

    add fetching tasks in module modal

--- frontend/src/components/ModuleModal/ModuleModal.js
+++ frontend/src/components/ModuleModal/ModuleModal.js
@@ -1,14 +1,38 @@
 import styles from "./ModuleModal.module.css";
+import { useEffect, useState } from "react";
 
 function Modal({ module, onClose }) {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        fetch(
+            `http://localhost:5000/task/getTaskGroupsByModuleId/${module._id}`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                setTasks(data);
+            })
+            .catch((error) => console.error(error));
+    }, [module._id]);
+    console.log(module._id);
+    console.log(tasks);
     return (
         <div className={styles.modalOverlay} onClick={onClose}>
             <div
                 className={styles.modalContent}
                 onClick={(e) => e.stopPropagation()}
             >
-                <h2>{module.name}</h2>
-                <p>{module.description}</p>
+                {tasks.map((tasks) => (
+                    <div key={tasks._id}>
+                        <h2>{tasks._id}</h2>
+                    </div>
+                ))}
                 <button onClick={onClose}>Закрити</button>
             </div>
         </div>
