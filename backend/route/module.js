9e5bb1cacd73058df3dbd83e16286dc08cf79ebd
ho4upitsu <ho4upitsu@gmail.com>
2025-03-02 11:19:16 +0200

    rewriten backen in nest.js

--- backend/route/module.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const moduleController = require("../controller/module");
-
-router.post("/createModule", moduleController.createModule);
-router.get("/getAllModules", moduleController.getAllModules);
-router.get("/getModule/:id", moduleController.getModule);
-router.delete("/deleteModule/:id", moduleController.deleteModule);
-
-module.exports = router;
