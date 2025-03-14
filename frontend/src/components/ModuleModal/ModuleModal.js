61d6656039a90914899545549bb8eaeb18403914
ho4upitsu <ho4upitsu@gmail.com>
2025-03-14 10:45:06 +0200

    add redirect to code editor from module modal

--- frontend/src/components/ModuleModal/ModuleModal.js
+++ frontend/src/components/ModuleModal/ModuleModal.js
@@ -1,27 +1,14 @@
 import styles from "./ModuleModal.module.css";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Modal({ module, onClose }) {
     const [lectule, setLecture] = useState([]);
     const [tasks, setTasks] = useState([]);
 
-    useEffect(() => {
-        fetch(
-            `http://localhost:5000/api/test-task/getTestTasksForModule/${module._id}`,
-            {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }
-        )
-            .then((response) => response.json())
-            .then((data) => {
-                setTasks(data);
-            })
-            .catch((error) => console.error(error));
-    }, [module._id]);
+    const navigate = useNavigate();
 
+    //fetching lecture
     useEffect(() => {
         console.log("Fetching lecture for module:", module?._id);
 
@@ -50,10 +37,30 @@ function Modal({ module, onClose }) {
 
         fetchLecture();
     }, [module?._id]);
+    //fetching tasks
+    useEffect(() => {
+        fetch(
+            `http://localhost:5000/api/test-task/getTestTasksForModule/${module._id}`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                setTasks(data);
+            })
+            .catch((error) => console.error(error));
+    }, [module._id]);
 
-    console.log(
-        `http://localhost:5000/api/lecture/getLectureForModule/${module._id}`
-    );
+    const redirectToCode = () => {
+        console.log("HUI");
+        navigate("/editor", { replace: true });
+
+        console.log("redirect");
+    };
 
     return (
         <div className={styles.modalOverlay} onClick={onClose}>
@@ -75,7 +82,7 @@ function Modal({ module, onClose }) {
                 <div className={styles.modalItem}>
                     <h2>Тестові завдання</h2>
                 </div>
-                <div className={styles.modalItem}>
+                <div className={styles.modalItem} onClick={redirectToCode}>
                     <h2>Випробовування кодом</h2>
                 </div>
                 <div className={styles.buttonContainer}>
